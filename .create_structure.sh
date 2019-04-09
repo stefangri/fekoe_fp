56a83c591ca59b90aecbd292f0827688bd6a8ca2
Stefan <stefan.grisard@tu-dortmund.de>
2019-04-09 12:34:44 +0200

    add template for lab report

---
+++ .create_structure.sh
@@ -0,0 +1,10 @@
+read -p "projectname: " dir_name
+read -p "title: " title
+read -p "date: " date_durchfuehrung
+
+cp -avr .templates/structure $dir_name
+
+
+echo "\newcommand{\versuch}{$title}" >> $dir_name/report/data.tex
+echo "\newcommand{\vd}{Tag der Durchführung: $date_durchfuehrung}" >> $dir_name/report/data.tex
+echo "\newcommand{\va}{Tag der Abgabe: $date_abgabe}" >> $dir_name/report/data.tex
