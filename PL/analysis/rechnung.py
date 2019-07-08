15ed4539a6556014bc92063e8335de0110b8b8ca
Julian Schröer <julian.schroeer@tu-dortmund.de>
2019-07-08 22:36:03 +0200

    pl korrekturen

--- PL/analysis/rechnung.py
+++ PL/analysis/rechnung.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.constants as const
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
 # import pint
 # ureg = pint.UnitRegistry()
 
@@ -13,9 +15,19 @@ mh = -0.45*const.m_e  # senkrecht
 # e_r = 9.29  # parallel
 e_r = 9.15  # senkrecht
 
-Epl1 = const.h*(1/(520*10**(-9)))*const.c
-Epl2 = const.h*(1/(580*10**(-9)))*const.c
-Epl3 = const.h*(1/(645*10**(-9)))*const.c
+
+######  Herstellerangaben
+# Epl1 = const.h*(1/(520*10**(-9)))*const.c
+# Epl2 = const.h*(1/(580*10**(-9)))*const.c
+# Epl3 = const.h*(1/(645*10**(-9)))*const.c
+
+#####  Gemessene Angaben
+l1 = ufloat(560.02, 0.02)
+l2 = ufloat(588.72, 0.04)
+l3 = ufloat(646.52, 0.03)
+Epl1 = const.h*(1/(l1*10**(-9)))*const.c
+Epl2 = const.h*(1/(l2*10**(-9)))*const.c
+Epl3 = const.h*(1/(l3*10**(-9)))*const.c
 # print(Eg-Epl1)
 # print(Epl1)
 # print(Epl3)
@@ -23,13 +35,13 @@ mu = me*mh/(mh+me)
 
 print(const.m_e)
 c1 = ((const.hbar * const.pi)**2) * (1/mh + 1/me)
-b1 = Eg - Epl1 - ((mu * const.e**4))/ 2 * (4 * const.pi * const.epsilon_0 * e_r * const.hbar)**2
+b1 = Eg - Epl1 - ((mu * const.e**4)) / 2 * (4 * const.pi * const.epsilon_0 * e_r * const.hbar)**2
 
 
 q1 = c1 / b1
 p1 = (1.786*const.e**2) / (8 * const.pi * e_r * const.epsilon_0 * (Eg - Epl1 - (mu * const.e**4) / (2*(4*const.pi*const.epsilon_0 * e_r * const.hbar)**2)))
 
-a1 = p1/2 + np.sqrt(p1**2 * 1/4 - q1)
+a1 = p1/2 + unp.sqrt(p1**2 * 1/4 - q1)
 print(a1)
 
 c2 = ((const.hbar * const.pi)**2) * (1/mh + 1/me)
@@ -39,7 +51,7 @@ b2 = Eg - Epl2 - ((mu * const.e**4)) / 2 * (4 * const.pi * const.epsilon_0 * e_r
 q2 = c2 / b2
 p2 = (1.786*const.e**2) / (8 * const.pi * e_r * const.epsilon_0 * (Eg - Epl2 - (mu * const.e**4) / (2*(4*const.pi*const.epsilon_0 * e_r * const.hbar)**2)))
 
-a2 = p2/2 + np.sqrt(p2**2 * 1/4 - q2)
+a2 = p2/2 + unp.sqrt(p2**2 * 1/4 - q2)
 print(a2)
 
 c3 = ((const.hbar * const.pi)**2) * (1/mh + 1/me)
@@ -49,7 +61,7 @@ b3 = Eg - Epl3 - ((mu * const.e**4)) / 2 * (4 * const.pi * const.epsilon_0 * e_r
 q3 = c3 / b3
 p3 = (1.786*const.e**2) / (8 * const.pi * e_r * const.epsilon_0 * (Eg - Epl3 - (mu * const.e**4) / (2*(4*const.pi*const.epsilon_0 * e_r * const.hbar)**2)))
 
-a3 = p3/2 + np.sqrt(p3**2 * 1/4 - q3)
+a3 = p3/2 + unp.sqrt(p3**2 * 1/4 - q3)
 print(a3)
 
 
